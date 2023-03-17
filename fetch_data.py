28ede8553e08fac0d54a0ea38c9ff45bde43ee67
zhaoqian <qian.zhao01@kuwo.cn>
2023-03-17 15:24:04 +0800

    1 fix文件抓取无序，按照时间先后排列整合数据 2 微信已更新接口数据，voteinfo重新拆解处理 3 fix中文不展示问题 simhei.ttf 4 直接运行在linux环境(centos) python2 生成vote.jpg 5 更新源请求url(最新投票) 6 运行顺序(test.py->fetch_data.py)

--- fetch_data.py
+++ fetch_data.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 import json
 from FileUtl import getFileList
+import os
+import natsort
+import matplotlib
+matplotlib.use('Agg')
 
 import numpy as np
 import pylab as pl
@@ -10,9 +14,12 @@ result = []
 fres = open('result.txt', 'w+')
 
 def getCurList():
-	file_name_list = getFileList('.')
+	return getdir()
+
+def getdir():
+	file_list = os.listdir("./vote")
+	file_name_list = sorted(file_list, key=lambda x:os.path.getmtime(os.path.join("./vote",x)))
 	for f in file_name_list:
-		print str(f)
 		if str(f)[0:4] == 'vote':
 			print "file: ", f, " prcessing..."
 			fetch_dic(f)
@@ -20,13 +27,14 @@ def getCurList():
 
 def fetch_dic(file_name):
 	# file_name = 'vote_2017_03_12__11_17_25.html'
+	file_name = "./vote/" + file_name
 	with open(file_name, 'r') as fr:
 		lnum = 0
 		for line in fr:
 			lnum += 1
-			if lnum == 816:
+			if lnum == 866:
 				# print line
-				stri = line[17:-2]
+				stri = line[19:-2]
 				# print stri
 				dicts = json.loads(stri)
 				# print dicts
@@ -40,7 +48,7 @@ def fetch_dic(file_name):
 					ite['name'] = item['name']
 					ite['cnt'] = item['cnt']
 					temp['options'].append(ite)
-				print temp
+				#print temp
 				result.append(temp)
 				fres.write(str(temp)+'\n')
 
@@ -65,6 +73,8 @@ def plt(result):
 	y14 = []
 	y15 = []
 	y16 = []
+	y17 = []
+	y18 = []
 
 	idx = 0
 	for epoch in result:
@@ -74,63 +84,77 @@ def plt(result):
 		x2.append(idx)
 		idx +=1
 		for op in epoch['options']:
-			if op['name'] == u'\u738b\u854a':
+			if op['name'] == u'\u73af\u5de5\u5b66\u9662 \u5218\u5b50\u8000':
 				y1.append(op['cnt'])
-			elif op['name'] == u'\u9646\u6021\u513f':
+			elif op['name'] == u'\u98df\u751f\u5b66\u9662 \u738b\u6668\u5b87':
 				y2.append(op['cnt'])
-			elif op['name'] == u'\u6881\u777f':
+			elif op['name'] == u'\u5316\u5de5\u5b66\u9662 \u738b\u82e5\u742a':
 				y3.append(op['cnt'])
-			elif op['name'] == u'\u4f59\u6b22\u5e86':
+			elif op['name'] == u'\u5316\u5de5\u5b66\u9662 \u5f6d\u7a0b':
 				y4.append(op['cnt'])
-			elif op['name'] == u'\u9648\u9e23\u79c0':
+			elif op['name'] ==  u'\u7406\u5b66\u9662 \u95eb\u5b50\u7487':
 				y5.append(op['cnt'])
-			elif op['name'] == u'\u51af\u7965\u541b':
+			elif op['name'] == u'\u7ecf\u7ba1\u5b66\u9662 \u5468\u65b0\u6960':
 				y6.append(op['cnt'])
-			elif op['name'] == u'\u9b4f\u6587\u5353':
+			elif op['name'] == u'\u7535\u6c14\u5b66\u9662 \u90b1\u8fd1\u7ff0':
 				y7.append(op['cnt'])
-			elif op['name'] == u'\u8521\u5723\u6770':
+			elif op['name'] == u'\u7406\u5b66\u9662 \u5f20\u96c5\u9759':
 				y8.append(op['cnt'])
-			elif op['name'] == u'\u9648\u96c5\u73fa':
+			elif op['name'] == u'\u4fe1\u606f\u5b66\u9662 \u5f20\u535a\u5b87':
 				y9.append(op['cnt'])
-			elif op['name'] == u'\u6731\u6021':
+			elif op['name'] == u'\u4e2d\u6fb3\u5b66\u9662 \u76f8\u4f73\u670b':
 				y10.append(op['cnt'])
-			elif op['name'] == u'\u5218\u73ba\u6726':
+			elif op['name'] == u'\u5efa\u5de5\u5b66\u9662 \u5434\u6768':
 				y11.append(op['cnt'])
-			elif op['name'] == u'\u5f20\u9e23\u9633':
+			elif op['name'] == u'\u827a\u672f\u5b66\u9662 \u674e\u6668':
 				y12.append(op['cnt'])
-			elif op['name'] == u'\u987e\u94ed\u8f69':
+			elif op['name'] == u'\u4fe1\u606f\u5b66\u9662 \u5218\u9e3f\u6bc5':
 				y13.append(op['cnt'])
-			elif op['name'] == u'\u7fdf\u73a5':
+			elif op['name'] == u'\u98df\u751f\u5b66\u9662 \u90ed\u6587\u9756':
 				y14.append(op['cnt'])
-			elif op['name'] == u'\u4faf\u6210\u6eaa':
+			elif op['name'] == u'\u673a\u68b0\u5b66\u9662 \u675c\u6625\u6656':
 				y15.append(op['cnt'])
-			elif op['name'] == u'\u5468\u4f73\u73ae':
+			elif op['name'] == u'\u5916\u56fd\u8bed\u5b66\u9662 \u8d75\u6021':
 				y16.append(op['cnt'])
+			
+			elif op['name'] == u'\u7406\u5b66\u9662 \u77f3\u7f8e':
+				y17.append(op['cnt'])
+			elif op['name'] == u'\u98df\u751f\u5b66\u9662 \u738b\u827a\u83b9':
+				y18.append(op['cnt'])
 
 
-	pl.plot(x1, y1, label=u'\u738b\u854a')# use pylab to plot x and y
-	pl.plot(x2, y2, label=u'\u9646\u6021\u513f')
-	pl.plot(x2, y3, label=u'\u6881\u777f')
-	pl.plot(x2, y4, label=u'\u4f59\u6b22\u5e86')
-	pl.plot(x2, y5, label=u'\u9648\u9e23\u79c0')
-	pl.plot(x2, y6, label=u'\u51af\u7965\u541b')
-	pl.plot(x2, y7, label=u'\u9b4f\u6587\u5353')
-	pl.plot(x2, y8, label=u'\u8521\u5723\u6770')
-	pl.plot(x2, y9, label=u'\u9648\u96c5\u73fa')
-	pl.plot(x2, y10, label=u'\u6731\u6021')
-	pl.plot(x2, y11, label=u'\u5218\u73ba\u6726')
-	pl.plot(x2, y12, label=u'\u5f20\u9e23\u9633')
-	pl.plot(x2, y13, label=u'\u987e\u94ed\u8f69')
-	pl.plot(x2, y14, label=u'\u7fdf\u73a5')
-	pl.plot(x2, y15, label=u'\u4faf\u6210\u6eaa')
-	pl.plot(x2, y16, label=u'\u5468\u4f73\u73ae')
 
+	pl.rcParams["font.sans-serif"]=["SimHei"] #设置字体
+	pl.rcParams["axes.unicode_minus"]=False #该语句解决图像中的“-”负号的乱码问题
+
+
+	pl.plot(x1, y1, label=u'\u73af\u5de5\u5b66\u9662 \u5218\u5b50\u8000')# use pylab to plot x and y
+	pl.plot(x2, y2, label=u'\u98df\u751f\u5b66\u9662 \u738b\u6668\u5b87')
+	pl.plot(x2, y3, label=u'\u5316\u5de5\u5b66\u9662 \u738b\u82e5\u742a')
+	pl.plot(x2, y4, label=u'\u5316\u5de5\u5b66\u9662 \u5f6d\u7a0b')
+	pl.plot(x2, y5, label=u'\u7406\u5b66\u9662 \u95eb\u5b50\u7487')
+	pl.plot(x2, y6, label=u'\u7ecf\u7ba1\u5b66\u9662 \u5468\u65b0\u6960')
+	pl.plot(x2, y7, label=u'\u7535\u6c14\u5b66\u9662 \u90b1\u8fd1\u7ff0')
+	pl.plot(x2, y8, label=u'\u7406\u5b66\u9662 \u5f20\u96c5\u9759')
+	pl.plot(x2, y9, label=u'\u4fe1\u606f\u5b66\u9662 \u5f20\u535a\u5b87')
+	pl.plot(x2, y10, label=u'\u4e2d\u6fb3\u5b66\u9662 \u76f8\u4f73\u670b')
+	pl.plot(x2, y11, label=u'\u5efa\u5de5\u5b66\u9662 \u5434\u6768')
+	pl.plot(x2, y12, label=u'\u827a\u672f\u5b66\u9662 \u674e\u6668')
+	pl.plot(x2, y13, label=u'\u4fe1\u606f\u5b66\u9662 \u5218\u9e3f\u6bc5')
+	pl.plot(x2, y14, label=u'\u98df\u751f\u5b66\u9662 \u90ed\u6587\u9756')
+	pl.plot(x2, y15, label=u'\u673a\u68b0\u5b66\u9662 \u675c\u6625\u6656')
+	pl.plot(x2, y16, label=u'\u5916\u56fd\u8bed\u5b66\u9662 \u8d75\u6021')
+	
+	pl.plot(x2, y17, label=u'\u7406\u5b66\u9662 \u77f3\u7f8e')
+	pl.plot(x2, y18, label=u'\u98df\u751f\u5b66\u9662 \u738b\u827a\u83b9')
 	pl.legend(loc='best')
 
-	pl.title(u'2017年3月12日 11:17:35 - 00:12:13 投票折线图')# give plot a title
+	pl.title(u'2023年3月16日 最美的你投票统计图')# give plot a title
 	pl.xlabel(u'采样点（次／10s）')# make axis labels
 	pl.ylabel(u'投票数')
 
+	print("-----------------")
+	pl.savefig('./vote.jpg')
 	pl.show()
 
 
@@ -138,7 +162,7 @@ def main():
 	# fetch_dic('vote_2017_03_12__11_17_25.html')
 	getCurList()
 	print '-------\n\n\n\n'
-	print result
+	# print result
 
 	# for epoch in result:
 	plt(result)
--- test.py
+++ test.py
@@ -8,12 +8,13 @@ def fetch():
 
 	fetcher=Fetcher(
 	user_agent='Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1', # 模拟浏览器的User-Agent
-	phantomjs_proxy='http://localhost:12366', # phantomjs的地址
+	phantomjs_proxy='http://localhost:12345', # phantomjs的地址
 	pool_size=10, # 最大的httpclient数量
 	async=False # 同步还是异步
 	)
 
-	url = "http://mp.weixin.qq.com/mp/newappmsgvote?action=show&__biz=MzIwMjcxMDkzNQ==&supervoteid=444598550&uin=&key=&pass_ticket=&wxtoken=&mid=2247484332&idx=1"
+	#url = "http://mp.weixin.qq.com/mp/newappmsgvote?action=show&__biz=MzIwMjcxMDkzNQ==&supervoteid=444598550&uin=&key=&pass_ticket=&wxtoken=&mid=2247484332&idx=1"
+	url = "http://mp.weixin.qq.com/mp/newappmsgvote?action=show&__biz=MzIwMTQ4Njc5MQ==&supervoteid=464183365&uin=MTU1MzY3NTIxNg==&key=809df1e076efc72f2f52bc4c512b2bbe92cd40fcdac7f1d7fdce154859e239d5fa42193a8cc3140256fd8aa2e3cbcf4d6fcbcd863eab2ce77bd4fc81e41dc9d375e6933c7867d5c3dc4abaf47684bcbd5228a0af814dee77b262e766fe43596f2eaae37edf78bf96ee50d26c5052c717f2f69c0d5faf57dfb86e4375155c563f&pass_ticket=DTcZT6F6sg6gk0w5Jp5ksbyZOaWyc0flneqm0Opjp4mJeqyRWb8D3Hwyri8+4MsBCSGP3NmfPoZARq/Dv+oHaA==&wxtoken=777&mid=2649626823&idx=1&appmsg_token="
 
 
 	temp = fetcher.phantomjs_fetch(url, js_script='function(){setTimeout("window.scrollTo(0,100000)}", 1000)')
@@ -22,7 +23,7 @@ def fetch():
 
 	print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()), " captured!", " -- coat time:", temp['time'], "s. "
 
-	file_name = "vote_"+time.strftime("%Y_%m_%d__%H_%M_%S", time.localtime())+".html"
+	file_name = "./vote/vote_"+time.strftime("%Y_%m_%d__%H_%M_%S", time.localtime())+".html"
 	f =open(file_name, 'w')
 	f.write(str(temp['content'].encode('utf-8')))
 	f.close()
